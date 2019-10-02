748b6aaad5288afae30890466946cfa541aa74a3
Ifoga John <45628841+eshy008@users.noreply.github.com>
2019-10-02 01:27:09 +0100

    Add files via upload

--- controllers/StudentController.php
+++ controllers/StudentController.php
@@ -1,19 +1,19 @@
 <?php
 
-require '../config/database.php';
+    require_once('../config/database.php') ;
+
 
     class Student {
 
+       
+        private $db;
+
+
         public function __construct(){
 
-            $this->db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-          
-            if(mysqli_connect_errno()){
-  
-              echo "Database connection failed with following errors:" . mysqli_connect_error();
-  
-              die();
-          }
+            $db = new DB;
+
+            $this->db = $db->get_connection();
 
         }
 
@@ -31,8 +31,15 @@ require '../config/database.php';
         public function RegisterCourses($courses_array, $id_number){
 
             $message = '';
+            $courses_arr = [];
 
-            $courses = json_encode($courses_array);
+            foreach($courses_array as $course){
+                if(!empty($course)){
+                    array_push($courses_arr, $course);
+                }
+            }
+
+            $courses = json_encode($courses_arr);
 
             $insert_sql = "UPDATE users SET courses = '$courses' WHERE id_number = '$id_number'";
 
@@ -50,17 +57,22 @@ require '../config/database.php';
 
         public function ShowRegdCourses($id_number){
 
-            $sql = "SELECT courses FROM users where id_number = '$id_number'";
+            $sql = "SELECT * FROM users where id_number = '$id_number'";
             $query = $this->db->query($sql);
 
-            $RegdCourses = mysqli_fetch_assoc($query);
+            $Result = mysqli_fetch_assoc($query);
+            $RegdCourses = $Result['courses'];
+            $RegdCourses = json_decode($RegdCourses);
 
             return $RegdCourses;
 
         }
 
         public function ShowCourseNotes($id_number){
-            $sql = "SELECT courses FROM users where id_number = '$id_number'";
+
+            $notes = [];
+
+            $sql = "SELECT * FROM users where id_number = '$id_number'";
             $query = $this->db->query($sql);
             $RegdCourses = mysqli_fetch_assoc($query);
 
@@ -73,14 +85,140 @@ require '../config/database.php';
                 $note_query = $this->db->query($note_sql);
 
                 $note = mysqli_fetch_assoc($note_query);
+                
+                if(!empty($note)){
 
-                return $note;
+                    array_push($notes, $note);
+                }
+                
 
             }
+            return $notes;
 
+        }
+
+        public function ShowCourseAssignments($id_number){
+
+            $assignments = [];
+
+            $sql = "SELECT * FROM users where id_number = '$id_number'";
+            $query = $this->db->query($sql);
+            $RegdCourses = mysqli_fetch_assoc($query);
+
+            $courses_offered = $RegdCourses['courses'];
+            $courses_offered = json_decode($courses_offered);
+
+            foreach($courses_offered as $course){
+
+                $assignment_sql = "SELECT * FROM assignments where course_code = '$course'";
+                $assignments_query = $this->db->query($assignment_sql);
+
+                $assignment = mysqli_fetch_assoc($assignments_query);
+                
+                if(!empty($assignment)){
+
+                    array_push($assignments, $assignment);
+                }
+                
+
+            }
+            return $assignments;
+
+        }
+
+        public function SubmitAssignment($course_code, $assignment_title,$target_file,$id_number){
+
+            $errors = array();
+            $messagesub = '';
+            $created_at = date('Y-m-d');
+            $grade = '';
+            $updated_at = '';
+
+            $insert_sql = "INSERT INTO submissions (submission_id,course_code,assignment_title,id_number,submission_file,created_at,grade, updated_at) 
+            VALUES(NULL,'$course_code','$assignment_title','$id_number','$target_file','$created_at','$grade', '$updated_at')" ;
+
+
+            if($insert_query = $this->db->query($insert_sql) ){
+
+                $messagesub = 'Assignment submitted successfully';
+                return $messagesub;
+
+            } else{
+                
+                die(mysqli_error($this->db));
+            }
+             return $messagesub;
 
 
         }
+        public function ShowAssignmentToLecturer($course_code){
+
+            $sql = "SELECT assignment_name FROM submit_assignment where course = '$course_code'";
+            $query = $this->db->query($sql);
+            $Courses = mysqli_fetch_assoc($query);
+            $courses = $Courses['course_code'];
+            $allcourse=[];
+
+            foreach ($courses as $item){
+                array_push($allcourse,$item);
+            }
+            return $allcourse;
+
+        }
+
+        public function ShowAssignmentToStudent($user_id){
+
+            $sql = "SELECT assignment_name FROM submit_assignment where user_id = '$user_id'";
+            $query = $this->db->query($sql);
+            $Courses = mysqli_fetch_assoc($query);
+            $course = $Courses['course_code'];
+
+            return $course;
+
+        }
+
+        public function GetUser($user_id){
+
+            $sql = "SELECT * FROM users where user_id = '$user_id'";
+            $query =$this->db->query($sql);
+
+            $user = mysqli_fetch_assoc($query);
+
+            return $user;
+          }
+
+        public function SelectCourses($faculty){
+
+            $sql ="SELECT * FROM courses WHERE faculty_code = '$faculty'";
+            $fac_courses = $this->db->query($sql);
+
+            return $fac_courses;
+
+        }
+        
+        public function SelectDepartments(){
+            
+            $sql ="SELECT * FROM departments";
+            $departments = $this->db->query($sql);
+
+            return $departments;
+        }
+        
+         public function SelectFaculties(){
+            
+            $sql ="SELECT * FROM faculties";
+            $faculties = $this->db->query($sql);
+
+            return $faculties;
+        }
+        
+        public function UpdateStudent($faculty_code, $dept_code,$id_number){
+            
+            $sql = "UPDATE users SET faculty_code = '$faculty_code', dept_code = '$dept_code' WHERE id_number = '$id_number'";
+            
+            $query = $this->db->query($sql) or die(mysqli_error($this->db));
+        }
+
     }
 
 
--- login.php
+++ login.php
@@ -1,64 +1,77 @@
 <?php
-  require 'controllers/user.php';
 
-  $user =  new User;
+require 'controllers/user.php';
+include('includes/header.php');
 
-  /*----to test login, create variables email and password with the same email 
-  and password you used to register just under this comment---*/
- 
+$user =  new User;
 
-  $message = $user->UserLogin($email,$password);
 
-  var_dump($message);
+if(isset($_SESSION['register_message'])){
 
+  $reg_message = $_SESSION['register_message'];
+
+}
+
+
+if (isset($_POST['login'])) {
+  $email = $_POST['email'];
+  $password =  $_POST['password'];
+
+
+  $message = $user->UserLogin($email, $password);
+}
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+  <div class="alert alert-sucess">
+      <img src="images/Image from iOS.png" alt="" srcset="" widht="100" height="70">
+  </div>
+  <h4 class="little-title text-center text-info">ASCLEPIUS CLASSROOM LOGIN AREA</h4>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/bootstrap.css">
-  <link rel="stylesheet" href="css/main.css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-  <title>Document</title>
-</head>
+  <div class="register_login-content" id="login-form">
+    <form action="login.php" method="POST">
+      <h2 class="form-title">Get Active</h2>
+      <h5 class="little-title text-center">Welcome back Student or Teacher Access your dashboard </h5>
 
-<body>
-  <header>
-    <div class="logo">
-      <img src="./images/Logo.png" alt="logo">
-    </div>
-  </header>
+      <div id="error_message">
+        <?php
+        if(!empty($reg_message)){
 
-  <main>
-    <h1>Get Active</h1>
-    <form class="login-form">
-      <div class="user-name">
-        <div class="icon">
-          <i class="fa fa-user" aria-hidden="true"></i>
-        </div>
-        <input type="text" placeholder="Username" required minlength="3">
+          echo "<div class='alert alert-success'>$reg_message</div>";
+
+          unset($_SESSION['register_message']);
+        }
+
+        if (!empty($message)) {
+
+          foreach ($message as $mess) {
+
+            echo "<div class='alert alert-danger'>$mess</div>";
+          }
+        }
+
+
+        ?>
       </div>
 
-      <div class="password">
-        <div class="icon">
-          <i class="fa fa-briefcase" aria-hidden="true"></i>
-        </div>
-        <input type="password" placeholder="Password" minlength="3" required>
+
+      <div class="group">
+        <label for="">Email</label>
+        <input type="email" name="email" id="email" class="text-input">
+        <span><img src="fonts/Vector.png" class="icon" alt=""></span>
       </div>
-
-      <input type="submit" value="LOGIN" class="btn">
-
-      <a href="#">Forget Password?</a>
-      <div class="line"></div>
+      <div class="group">
+        <label for="">Password</label>
+        <input type="password" name="password" id="password" class="text-input">
+        <span><img src="fonts/Vector-1.png" class="icon" alt=""></span>
+      </div>
+      <div>
+        <button type="submit" class="btn btn-big" name="login">Login</button>
+      </div>
+      <p>Don't have an account? <a href="register.php" class="text-danger">Register</a></p>
       
     </form>
-  </main>
+  </div>
 
   <script src="js/main.js"></script>
 </body>
